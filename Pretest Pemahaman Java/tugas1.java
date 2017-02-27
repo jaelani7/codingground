1f3dc220dfbe17e7d547b900d25450ab9d490724
jaelani7 <jailaniahmad80@gmail.com>
2017-02-27 11:55:07 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas1.java
@@ -0,0 +1,8 @@
+public class tugas1{
+    
+    public static void main (String [] args){
+        String nama ="jaelani";
+        String kota ="Jepara";
+        System.out.println("Nama : "+nama+"\nKota : "+kota);
+    }
+}
