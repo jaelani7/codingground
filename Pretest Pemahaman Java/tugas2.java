0b43c9d9b4f439b7840a31c39f1badf41f970b4e
jaelani7 <jailaniahmad80@gmail.com>
2017-02-27 11:55:58 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas2.java
@@ -0,0 +1,10 @@
+public class tugas2{
+
+     public static void main(String []args){
+        String[] charArray ={ "ahmad", "jaelani", "jae" }; 
+        
+        for (int i = 0; i < charArray.length; i++) {
+         System.out.println(charArray[i] + " ");
+        }
+     }
+}
