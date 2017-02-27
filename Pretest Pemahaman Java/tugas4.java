1967ef5cad9ec8b89835d4b898c5fa0a7f413273
jaelani7 <jailaniahmad80@gmail.com>
2017-02-27 11:55:08 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas4.java
@@ -0,0 +1,14 @@
+public class tugas4{
+
+     public static void main(String []args){
+        int parameter = 1;
+        int hasil = Penjumlahan(parameter);
+        System.out.println("Angka penjumlahan " + parameter + " + 2 = " + hasil);
+     }
+     
+     public static int Penjumlahan(int x1) {
+      int angka = x1;
+
+      return angka + 2; 
+   }
+}
