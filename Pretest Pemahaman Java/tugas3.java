c756e2f9594ef85c8f4ecec0a0ff9eaf786e6e33
jaelani7 <jailaniahmad80@gmail.com>
2017-02-27 11:54:15 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas3.java
@@ -0,0 +1,14 @@
+public class tugas3{
+
+     public static void main(String []args){
+        int x = 3;
+        int y = OutputAngka(x);
+        System.out.println("Angka yang anda masukkan adalah angka " + y);
+     }
+     
+     public static int OutputAngka(int x1) {
+      int angka = x1;
+      
+      return angka; 
+   }
+}
